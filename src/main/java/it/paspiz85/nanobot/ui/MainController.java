f2a254ad68f92033bd8cca0483c1583014934238
Pasquale Pizzuti <v-ppizzuti@ROMC02MJ20NFD58.sea.corp.expecn.com>
2015-06-26 21:57:20 +0200

    test

--- src/main/java/it/paspiz85/nanobot/ui/MainController.java
+++ src/main/java/it/paspiz85/nanobot/ui/MainController.java
@@ -26,6 +26,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -215,6 +216,7 @@ public class MainController implements ApplicationAwareController, Constants {
     @FXML
     public void handleStartButtonAction() {
         if (runnerService.getState() == State.READY) {
+            setupResolution();
             runnerService.start();
         }
     }
