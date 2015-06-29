c583beb65f450d8b69e4af02ef28256cf0983ad0
Pasquale Pizzuti <v-ppizzuti@ROMC02MJ20NFD58.sea.corp.expecn.com>
2015-06-29 15:29:26 +0200

    fix attack but area

--- src/main/java/it/paspiz85/nanobot/parsing/Area.java
+++ src/main/java/it/paspiz85/nanobot/parsing/Area.java
@@ -13,7 +13,7 @@ public enum Area implements Constants {
     ATTACK_GROUP(new Point(24, 554), new Point(836, 653)), BARRACKS_BUTTONS(new Point(188, 581), new Point(679, 679)), ENEMY_BASE(
             new Point(31, 0), new Point(831, 510)), ENEMY_LOOT(new Point(17, 68), new Point(138, 240)), FULLSCREEN(
                     new Point(0, 0), new Point(BS_RES_X - 1, BS_RES_Y - 1)), ATTACK_BUTTON(
-                            new Point(0, 0), new Point(BS_RES_X - 1, BS_RES_Y - 1));
+                            new Point(25, 634), new Point(100, 652));
 
     private final Point p1;
 
