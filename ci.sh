5bfed49062b60e1d124a94b97d11b99b32f287d3
Pasquale Pizzuti <v-ppizzuti@ROMC02MJ20NFD58.sea.corp.expecn.com>
2015-07-03 16:42:01 +0200

    fixed ci

--- ci.sh
+++ ci.sh
@@ -3,13 +3,29 @@ FOLDER="/tmp/nanobot"
 GIT_URL="https://github.com/paspiz85/nanobot.git"
 GIT_BRANCH="feature/collecting"
 
+function notify {
+	OS=$(uname)
+	if [[ $OS == 'Darwin' ]]; then
+		osascript -e "display dialog \"$1\""
+	elif [[ $OS == 'Linux' ]]; then
+		echo $1
+	else
+		echo $1
+	fi
+}
+
 rm -rf $FOLDER
 git clone $GIT_URL $FOLDER
 pushd $FOLDER
 {
 	git checkout $GIT_BRANCH
 	mvn clean install
-	echo $?
+	RESULT=$?
+	if [[ $RESULT != 0 ]]; then
+		notify "Build nanobot failed"
+	#else
+		#notify "Build nanobot OK"
+	fi
 } || {
 	popd
 	rm -rf $FOLDER
