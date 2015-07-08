bfc5d82e71dd4df03031fbc2a69a262dda313337
Pasquale Pizzuti <v-ppizzuti@ROMC02MJ20NFD58.sea.corp.expecn.com>
2015-07-08 13:11:23 +0200

    fix ci

--- ci.sh
+++ ci.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 FOLDER="/tmp/nanobot"
+LOGFILE="/tmp/nanobot.log"
 GIT_URL="https://github.com/paspiz85/nanobot.git"
 GIT_BRANCH="feature/collecting"
 
@@ -7,7 +8,6 @@ function notify {
 	OS=$(uname)
 	if [[ $OS == 'Darwin' ]]; then
 		osascript -e 'tell application "Finder"' -e 'activate' -e "display dialog \"$1\"" -e 'end tell'
-		#osascript -e "display dialog \"$1\""
 	elif [[ $OS == 'Linux' ]]; then
 		echo $1
 	else
@@ -20,14 +20,15 @@ git clone $GIT_URL $FOLDER
 pushd $FOLDER
 {
 	git checkout $GIT_BRANCH
-	mvn clean install
+	mvn clean install > $LOGFILE
 	RESULT=$?
 	if [[ $RESULT != 0 ]]; then
-		notify "Build nanobot failed"
+		notify "Build nanobot failed: see $LOGFILE"
 	#else
 		#notify "Build nanobot OK"
 	fi
 } || {
-	popd
-	rm -rf $FOLDER
+        notify "Build nanobot exception"
 }
+popd
+rm -rf $FOLDER
