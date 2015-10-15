c6a5ebd3b9605cb803178ca435b029922bbb4021
Pasquale Pizzuti <paspiz85@gmail.com>
2015-10-15 22:04:42 +0200

    loop script

--- scripts/_loop.js
+++ scripts/_loop.js
@@ -1,6 +1,6 @@
 var StateMainMenu = Java.type('it.paspiz85.nanobot.logic.StateMainMenu');
 
-var active = true;
+var active = false;
 if (active) {
 	if (context.getTrainCount() % 6 == 1) {
 		StateMainMenu.instance().postMessage('RECLUTO IN CLAN DI SOLI MAGGIORENNI. NO BM. FATE RICHIESTA!');
