e66962a77fe6e94694e20ac8870d0846db165273
Pasquale Pizzuti <paspiz85@gmail.com>
2015-10-15 22:01:27 +0200

    loop script

--- scripts/_loop.js
+++ scripts/_loop.js
@@ -1,10 +1,10 @@
 var StateMainMenu = Java.type('it.paspiz85.nanobot.logic.StateMainMenu');
 
-var active = false;
+var active = true;
 if (active) {
-	if (context.getTrainCount() % 2 == 1) {
-		StateMainMenu.instance().postMessage('Recluto in clan di soli adulti');
-	} else {
-		StateMainMenu.instance().postMessage('Recluto in clan di soli adulti.');
+	if (context.getTrainCount() % 6 == 1) {
+		StateMainMenu.instance().postMessage('RECLUTO IN CLAN DI SOLI MAGGIORENNI. NO BM. FATE RICHIESTA!');
+	} else if (context.getTrainCount() % 6 == 4) {
+		StateMainMenu.instance().postMessage('RECLUTO PER CLAN DI SOLI MAGGIORENNI. NO BM. FATE RICHIESTA!');
 	}
 }
