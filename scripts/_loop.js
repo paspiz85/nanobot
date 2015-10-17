62f36aac3315393f39a9eb970d708afa26cc039b
Pasquale Pizzuti <paspiz85@gmail.com>
2015-10-17 16:49:45 +0200

    changed message logic

--- scripts/_loop.js
+++ scripts/_loop.js
@@ -1,4 +1,4 @@
-int n = 5;
+var n = 3;
 if (context.getTrainCount() % (2*n) == 1) {
 	postMessage('RECLUTO IN CLAN DI SOLI MAGGIORENNI. NO BM. FATE RICHIESTA!', false);
 } else if (context.getTrainCount() % (2*n) == n+1) {
